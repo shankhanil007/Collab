6e7c398b76edc75890485622185ee8ffd2f57826
siddharth85 <you@example.com>
2021-05-23 18:27:05 +0530

    new files added

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+
+const app=express();
+app.use(express.static('public'));
+app.listen(3000);
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
