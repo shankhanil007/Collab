71f931958c4354c460d4e45fb3b48e1ce45d492b
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-23 19:33:18 +0530

    Added Files

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!!");
